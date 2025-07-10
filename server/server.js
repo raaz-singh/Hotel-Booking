68fcc01ce7483e9a2f789fd2daa54fdc13e6546c
Roubin Raj <151029462+raaz-singh@users.noreply.github.com>
2025-07-10 07:27:12 +0530

    new10

--- server/server.js
+++ server/server.js
@@ -4,8 +4,14 @@ import cors from "cors"
 import connectDB from "./configs/db.js";
 import { clerkMiddleware } from '@clerk/express'
 import clerkWebhooks from "./controllers/clerkWebhooks.js";
+import userRouter from "./routers/userRouter.js";
+import hotelRouter from "./routers/hotelRouters.js";
+import connectCloudinary from "./configs/cloudinary.js";
+import roomRouter from "./routers/roomRoutes.js";
+import bookingRouter from "./routers/bookingRoute.js";
 
 connectDB()
+connectCloudinary();
 
 const app = express()
 app.use(cors()) //Enable Cross-Origin Resource Sharing
@@ -18,6 +24,10 @@ app.use(clerkMiddleware())
 app.use("/api/clerk", clerkWebhooks);
 
 app.get('/',(req,res)=>res.send("API is Working"))
+app.use('/api/user',userRouter)
+app.use('/api/hotels',hotelRouter)
+app.use('/api/rooms',roomRouter)
+app.use('/api/bookings',bookingRouter)
 
 const PORT = process.env.PORT || 3000;
 
